2cb629d6b5735cb6cba699278c397d45c5cd973a
roiaki <roitoitoitoi@yahoo.co.jp>
2023-01-16 23:21:54 +0900

    一旦ここまで

---
+++ src/resources/views/livewire-test/index.blade.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    @livewireStyles
+  </head>
+  <body>
+    livewireテスト
+    <div>
+      @if (session()->has('message'))
+        <div class="alert alert-success">
+          {{ session('message') }}
+        </div>
+      @endif
+    </div>
+    <livewire:counter />
+
+    @livewireScripts
+  </body>
+</html>
